2622e15a5b9af0ef456cad878604a1780e33a6c8
Eddie <81389737+MexaGit@users.noreply.github.com>
2026-07-31 16:45:55 -0600

    Create hola_lenguaje.py

---
+++ hola_lenguaje.py
@@ -0,0 +1,9 @@
+def main():
+    nombre = "Eduardo"
+    lenguaje = "Python"
+
+    print(f"Hola {nombre}!")
+    print(f"Tu lenguaje favorito es {lenguaje}.")
+
+if __name__ == "__main__":
+    main()
